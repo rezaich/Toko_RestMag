06bed8703aba903745d5ed6aa4774c1ea3a97c65
reza <ichsanireza@gmail.com>
2021-06-10 13:51:36 +0700

    NEW COMMIT FOR -Create New Fragment and activity

--- app/src/main/java/com/zaich/toko_restmag/RegisterActivity.kt
+++ app/src/main/java/com/zaich/toko_restmag/RegisterActivity.kt
@@ -1,11 +1,17 @@
 package com.zaich.toko_restmag
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        val intent = Intent(application,LoginActivity::class.java)
+        btRegis.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/zaich/toko_restmag/fragment/Akun.kt
+++ app/src/main/java/com/zaich/toko_restmag/fragment/Akun.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.zaich.toko_restmag.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CreateUser.newInstance] factory method to
+ * Use the [Akun.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CreateUser : Fragment() {
+class Akun : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class CreateUser : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_user, container, false)
+        return inflater.inflate(R.layout.fragment_akun, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class CreateUser : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment CreateUser.
+         * @return A new instance of fragment Akun.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CreateUser().apply {
+            Akun().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
